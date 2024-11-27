7fa46ded128dd474726fb2a5bedb0e5bf902d0bf
g1g2 <g1g2@g1g2.xyz>
2024-11-27 07:45:10 -0800

    fix

--- src/sql/db_connection_pool/duckdbpool.rs
+++ src/sql/db_connection_pool/duckdbpool.rs
@@ -228,14 +228,6 @@ fn test_connection(conn: &r2d2::PooledConnection<DuckdbConnectionManager>) -> Re
 
 fn get_config(access_mode: &AccessMode) -> Result<duckdb::Config> {
     let config = duckdb::Config::default()
-        .max_memory("16GB")?
-        .threads(20)?
-        .default_null_order(DefaultNullOrder::NullsLast)?
-        .default_order(DefaultOrder::Desc)?
-        .enable_external_access(true)?
-        .enable_object_cache(false)?
-        .enable_autoload_extension(true)?
-        .allow_unsigned_extensions()?
         .access_mode(match access_mode {
             AccessMode::ReadOnly => duckdb::AccessMode::ReadOnly,
             AccessMode::ReadWrite => duckdb::AccessMode::ReadWrite,
